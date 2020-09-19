5be8481c0fabb075d4f1ef0baf177885b9bbc828
Giov4 <brancacciogiovanni1@gmail.com>
2020-09-19 20:43:07 +0000

    Deleted old map_create.lua

--- _map_handler/map_create.lua
+++
@@ -1,8 +0,0 @@
--- Creates the schematic using pos1 (the first corner of the map) and pos2 (the second one)
-function skywars.create_schematic(sender, pos1, pos2, name, arena)
-    minetest.create_schematic(pos1, pos2, nil, minetest.get_worldpath() .. "/" .. name .. ".mts", nil)
-    arena.schematic = minetest.get_worldpath() .. "/" .. name .. ".mts"
-
-    arena_lib.change_arena_property(sender, "skywars", arena.name, "pos1", pos1) 
-    arena_lib.change_arena_property(sender, "skywars", arena.name, "schematic", arena.schematic)
-end
